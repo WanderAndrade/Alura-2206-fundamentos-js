e9e7d7cbbe8fddc0c0842d851d44c832cb8cdec7
WanderAndrade <wanderdeveloper01@gmail.com>
2021-06-23 01:54:49 -0300

    envio do aruivo testes

---
+++ testes.js
@@ -0,0 +1,16 @@
+const idade	= 2;
+const nome	= "Lara";
+const sobrenome = "Ayumi";
+const bacharel	= false;
+const dezAnosDepois = "12";
+
+const nomeCompleto = nome + sobrenome;
+
+console.log(nomeCompleto);
+console.log(bacharel == 0);
+const minhaVar = 1;
+const MinhaVar = "texto";
+const minhavar = "3";
+const MINHAVAR = 2;
+
+console.log(minhaVar, MinhaVar, minhavar, MINHAVAR)
